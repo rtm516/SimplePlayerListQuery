131bae63531aa02431de3640da98449e588051c3
Ryan Milner <ryantmilner@hotmail.co.uk>
2019-08-05 23:20:45 +0100

    Finished plugin for release

--- src/main/java/com/rtm516/SimplePlayerListQuery/QueryEvent.java
+++ src/main/java/com/rtm516/SimplePlayerListQuery/QueryEvent.java
@@ -9,9 +9,14 @@ import net.md_5.bungee.api.ServerPing;
 import net.md_5.bungee.api.ServerPing.PlayerInfo;
 import net.md_5.bungee.api.connection.ProxiedPlayer;
 import net.md_5.bungee.api.event.ProxyPingEvent;
+import net.md_5.bungee.api.plugin.Listener;
 import net.md_5.bungee.event.EventHandler;
 
-public class QueryEvent {
+public class QueryEvent implements Listener {
+	public QueryEvent() {
+	    SimplePlayerListQuery.instance.getProxy().getPluginManager().registerListener(SimplePlayerListQuery.instance, this);
+  }
+	
 	@EventHandler(priority = 32)
 	public void onServerPing(ProxyPingEvent e) {
 		Collection<ProxiedPlayer> players = ProxyServer.getInstance().getPlayers();
@@ -20,10 +25,11 @@ public class QueryEvent {
 			e.getResponse().getPlayers().setSample(null);
 		}
 
-		List<ServerPing.PlayerInfo> playerInfo = new ArrayList<ServerPing.PlayerInfo>();
-		for (ProxiedPlayer player : players) {
-			playerInfo.add(new ServerPing.PlayerInfo(player.getDisplayName(), player.getUniqueId()));
-		}
-		e.getResponse().getPlayers().setSample((PlayerInfo[]) playerInfo.toArray());
+		ServerPing.PlayerInfo[] playerInfo = new ServerPing.PlayerInfo[players.size()];
+	    for (int i = 0; i < playerInfo.length; i++) {
+	    	ProxiedPlayer player = (ProxiedPlayer) players.toArray()[i];
+	    	playerInfo[i] = new ServerPing.PlayerInfo(player.getDisplayName(), player.getUniqueId());
+	    }
+		e.getResponse().getPlayers().setSample(playerInfo);
 	}
 }
